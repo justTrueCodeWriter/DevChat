1a35f6f7cc5fb6f331809312a35a04ec0f7f97e0
Dmitry <81821203+justTrueCodeWriter@users.noreply.github.com>
2023-02-08 22:55:32 +0300

    Add files via upload

--- transmitter.cpp
+++ transmitter.cpp
@@ -2,8 +2,9 @@
 #include <string>
 
 void user_login(char *username);
-void user_message(char *username, char *message);
+int user_message(char *username, char *message);
 void user_message_to_file(char *username, char *message);
+void system_message(char *username, char *message);
 
 
 int main() {
@@ -11,9 +12,12 @@ int main() {
 	char username[20];
 	char message[300];
 	char messageToTextFile[323];
+	int systemMessage;
 	user_login(username);
 	while (true) {
-		user_message(username, message);
+		systemMessage = user_message(username, message);
+		if (systemMessage == -1) 
+			return 0;
 		user_message_to_file(username, message);
 	}
 	//printf("%s", username);
@@ -21,11 +25,19 @@ int main() {
 }
 
 void user_login(char *username) {
-	scanf("%s", username);
+	char sys[] = "joined chat";
+	printf("login: "); scanf("%s", username);
+	system_message(username, sys);
 }
 
-void user_message(char *username, char *message) {
+int user_message(char *username, char *message) {
 	printf("[%s]: ", username); scanf("%s", message);
+	char sys[] = "left chat";
+	if (message[0] == 'q') {
+		system_message(username, sys);
+		return -1;		
+	}
+	return 0;
 }
 
 void user_message_to_file(char *username, char *message) {
@@ -33,6 +45,16 @@ void user_message_to_file(char *username, char *message) {
 
 	fp = fopen("chat", "a");
 	fprintf(fp, "[%s]: %s\n", username, message);
+	fclose(fp);
+
+}
+
+void system_message(char *username, char *message) {
+
+	FILE *fp;
+
+	fp = fopen("chat", "a");
+	fprintf(fp, "SYSTEM: [%s] %s\n", username, message);
 	fclose(fp);
 
 }
